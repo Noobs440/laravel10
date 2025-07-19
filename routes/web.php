9c0c9dfc51f4aed7d293290a9e3a12d1c5bfd162
Kamte MBA <hyancintheurbainkamtemba@gmail.com>
2025-07-19 10:54:26 +0200

    enjeu

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Usecases\AuthController;
+use App\Http\Controllers\Usecases\Authcontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +19,4 @@ use App\Http\Controllers\Usecases\AuthController;
 // });
 
 
-Route::get('user/verify/{token}', [AuthController::class, 'verifyAccount'])->name('user.verify');
-
+Route::get('user/verify/{token}', [Authcontroller::class, 'verifyAccount'])->name('user.verify');
