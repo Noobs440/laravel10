047df5ca9f3f212dff69c8d7a4c42832f8632ce7
Kamte MBA <hyancintheurbainkamtemba@gmail.com>
2025-07-19 09:53:32 +0200

    test44

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Usecases\Authcontroller;
+use App\Http\Controllers\Usecases\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +19,5 @@ use App\Http\Controllers\Usecases\Authcontroller;
 // });
 
 
-Route::get('user/verify/{token}', [Authcontroller::class, 'verifyAccount'])->name('user.verify');
+Route::get('user/verify/{token}', [AuthController::class, 'verifyAccount'])->name('user.verify');
 
